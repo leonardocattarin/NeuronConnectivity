d2bb3922eb1017a25be9f34a38f3462b7068c83d
Leonardo <leonardo.cattarin@gmail.com>
2022-10-22 17:53:01 +0200

    More noisy but more complete

--- utilities/funcs.py
+++ utilities/funcs.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def get_biggest_object (image):
     image = image.astype('uint8')
-    nb_components, output, stats, centroids = cv.connectedComponentsWithStats(image, connectivity=4)
+    nb_components, output, stats, centroids = cv.connectedComponentsWithStats(image, connectivity=8)
     sizes = stats[:, -1]
 
     max_label = 1
@@ -16,3 +16,26 @@ def get_biggest_object (image):
     img2 = np.zeros(output.shape)
     img2[output == max_label] = 1
     return img2
+
+def process_img (img):
+    ret,temp_img = cv.threshold(img,60,1,cv.THRESH_TOZERO)
+    
+    kernel_cl1 = np.ones((19,19),np.uint8)
+    temp_img = cv.morphologyEx(temp_img, cv.MORPH_DILATE, kernel_cl1)
+
+    temp_mask = get_biggest_object(temp_img)
+    temp_img = temp_img*temp_mask
+
+    kernel_cl1 = np.ones((19,19),np.uint8)
+    temp_img = cv.morphologyEx(temp_img, cv.MORPH_ERODE, kernel_cl1)
+
+    kernel_op1 = np.ones((6,6),np.uint8)
+    temp_img = cv.morphologyEx(temp_img, cv.MORPH_OPEN, kernel_op1)
+
+    kernel_cl1 = np.ones((19,19),np.uint8)
+    temp_img = cv.morphologyEx(temp_img, cv.MORPH_CLOSE, kernel_cl1)
+
+    ret,final_mask = cv.threshold(temp_img,20,1,cv.THRESH_BINARY)
+
+
+    return final_mask
